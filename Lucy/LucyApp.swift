c3b26e9b710eaafd560b77566a4c428702b695ba
Ayush <ayushdesai1000@gmail.com>
2024-07-16 21:56:01 +0530

    Initial Commit

---
+++ Lucy/LucyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LucyApp.swift
+//  Lucy
+//
+//  Created by Ayush Desai on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct LucyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
